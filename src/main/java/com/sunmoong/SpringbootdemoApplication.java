e2b23f75f661b2e59e47ecc714971fd3496e74c2
YOUJ <826104943@qq.com>
2018-04-16 18:35:01 +0800

    commit

--- src/main/java/com/sunmoong/SpringbootdemoApplication.java
+++ src/main/java/com/sunmoong/SpringbootdemoApplication.java
@@ -1,12 +1,36 @@
 package com.sunmoong;
 
+import com.sunmoong.config.AuthorSettings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
+
+	@Value("${book.author}")
+	private String bookAuthor;
+
+	@Value("${book.name}")
+	private String bookName;
+
+	@Autowired
+	private AuthorSettings authorSettings;
+
+	@RequestMapping("/")
+	String index(){
+		return "Hello Spring Boot"+ bookAuthor+":"+bookName+"and author age is "+ authorSettings.getAge();
+	}
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootdemoApplication.class, args);
+		SpringApplication app = new SpringApplication(SpringbootdemoApplication.class);
+		app.run(args);
+//		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
 }
---
+++ src/main/java/com/sunmoong/config/AuthorSettings.java
@@ -0,0 +1,32 @@
+package com.sunmoong.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by G05326 on 2018/4/16.
+ */
+@Component
+@ConfigurationProperties(ignoreUnknownFields = false,prefix = "author")
+public class AuthorSettings {
+
+    private String name;
+    private Long age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+}
